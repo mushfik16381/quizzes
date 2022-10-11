19ca35c5702f3eff1c5a86770ec8ce47d85d7def
mushfik16381 <ahmed.mushfik16381@gmail.com>
2022-10-12 04:34:49 +0600

    statistics part

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './Ouestion.css'
 
 const Question = ({ques}) => {
-    console.log(ques)
+    // console.log(ques)
     return (
-        <div>
+        <div className='qus-container'>
             <h3>{ques.question}</h3>
+            <div className='ans-container'>
+            <h3>{ques.options[0]}</h3>
+            <h3>{ques.options[1]}</h3>
+            <h3>{ques.options[2]}</h3>
+            <h3>{ques.options[3]}</h3>
+            </div>
+            {/* <div>
+            
+            </div> */}
         </div>
     );
 };
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { useLoaderData } from 'react-router-dom';
 
 const Statistics = () => {
+    const cart = useLoaderData().data;
+    const {name, total} = cart([])
+    console.log(cart[0].name);
     return (
         <div>
-            <h3>Statistics</h3>
+            <LineChart width={500} height={400} data={cart}>
+            <Line type="monotone"  dataKey={total} stroke="#82ca9d" />
+            <XAxis dataKey={cart[0].name} />
+            <YAxis />
+            </LineChart>
         </div>
     );
 };
