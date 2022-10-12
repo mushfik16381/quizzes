cf65e95c58ec2bc647a7fd590ad2fa9023b97680
mushfik16381 <ahmed.mushfik16381@gmail.com>
2022-10-12 19:13:38 +0600

    qus design

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,16 +1,37 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 import './Ouestion.css'
 
 const Question = ({ques}) => {
-    // console.log(ques)
+    console.log(ques)
     return (
         <div className='qus-container'>
-            <h3>{ques.question}</h3>
+            <div className='qus-eye'>
+                <h3>Quiz :  {ques.question}</h3>
+                <span className='icon-eye'><FontAwesomeIcon icon={faEye} /></span>
+            </div>
             <div className='ans-container'>
-            <h3>{ques.options[0]}</h3>
-            <h3>{ques.options[1]}</h3>
-            <h3>{ques.options[2]}</h3>
-            <h3>{ques.options[3]}</h3>
+                <div className='mcq'>
+                    <input type="radio" />
+                    <label ><p>{ques.options[0]}</p></label>
+                </div>
+                <div className='mcq'>
+                    <input type="radio" />
+                    <label ><p>{ques.options[1]}</p></label>
+                </div>
+                <div className='mcq'>
+                    <input type="radio" />
+                    <label ><p>{ques.options[2]}</p></label>
+                </div>
+                <div className='mcq'>
+                    <input type="radio" />
+                    <label ><p>{ques.options[3]}</p></label>
+                </div>
+                
+                
+                
+                
             </div>
             {/* <div>
             
